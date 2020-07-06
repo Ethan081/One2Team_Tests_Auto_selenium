dd7c3bb0bc5f053d6cda932bc8f2f1d787fff606
Devillers <devillerspm@gmail.com>
2020-07-06 16:10:25 +0200

    Create new card

--- src/test/java/org/one2Team/BoardStepdefs.java
+++ src/test/java/org/one2Team/BoardStepdefs.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.*;
@@ -71,6 +73,23 @@ public class BoardStepdefs {
         assertEquals(driver.findElement(By.id("/ogp/8558138")), driver.findElement(By.id("/ogp/8558138")));
 
     }
- */
+*/
 
+    @And("user is on board page")
+    public void userIsOnBoardPage() throws InterruptedException {
+        driver.findElement(By.cssSelector("#root > div > section > aside > div > div > div.module-links-list > a:nth-child(2)")).click();
+        assertTrue(driver.getCurrentUrl().contains("slideboard-new/a/"));
+        Thread.sleep((5000));
+    }
+
+    @When("user creates a new card")
+    public void userCreatesANewCard() throws InterruptedException {
+        driver.findElement(By.cssSelector("#columns-container > div > div.containCol__col.column-animation.col0 > div.containCol__col__plus > div.header.containCol__col__header.ng-scope > div > span")).click();
+        DateTimeFormatter dtf  = DateTimeFormatter.ofPattern("yyyyMMddHHmmssSSS");
+        LocalDateTime ldt = LocalDateTime.now();
+        String dateDuJour = ldt.format(dtf);
+        driver.findElement(By.cssSelector("body > div:nth-child(8) > div > div.ant-modal-wrap > div > div.ant-modal-content > div.ant-modal-body > div > div.ant-row.ant-form-item > div.ant-col.ant-form-item-control-wrapper > div > span > input")).sendKeys("new card" + dateDuJour);
+        driver.findElement(By.cssSelector("body > div:nth-child(8) > div > div.ant-modal-wrap > div > div.ant-modal-content > div.ant-modal-footer > div > div.footer-action-buttons > button.ant-btn.ant-btn-primary")).click();
+        Thread.sleep((5000));
+    }
 }
